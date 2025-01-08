2aad44d7b7eb4d1968cb0cab22e526707cd2f37a
RakodilGena <rakodilgenathefirst@gmail.com>
2025-01-08 17:19:24 +0300

    userId fixed

--- Server/Doco.Server/Doco.Server.Gateway.Dal/Migrations/Initial_1.cs
+++ Server/Doco.Server/Doco.Server.Gateway.Dal/Migrations/Initial_1.cs
@@ -5,25 +5,24 @@ namespace Doco.Server.Gateway.Dal.Migrations;
 [Migration(1)]
 public sealed class Initial_1 : Migration
 {
-    // Guid Id
-    // string Name
-    // string Mail
-    // bool IsAdmin
-    // timestamptz DeletedAt nullable
     public override void Up()
     {
         Create.Table("users")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().Identity()
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey()
             .WithColumn("name").AsString().NotNullable()
-            .WithColumn("mail").AsString().NotNullable()
+            .WithColumn("email").AsString().NotNullable()
             .WithColumn("hashed_password").AsString().NotNullable()
             .WithColumn("hash_password_salt").AsString().NotNullable()
             .WithColumn("is_admin").AsBoolean().NotNullable()
+
+            .WithColumn("created_at").AsCustom("timestamp with time zone")
+            .NotNullable().WithDefault(SystemMethods.CurrentUTCDateTime)
+
             .WithColumn("deleted_at").AsCustom("timestamp with time zone").Nullable();
 
-        Create.Index("idx_user_mail")
+        Create.Index("idx_user_email")
             .OnTable("users")
-            .OnColumn("mail")
+            .OnColumn("email")
             .Ascending()
             .WithOptions()
             .Unique();
